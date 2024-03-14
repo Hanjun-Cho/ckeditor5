64682dcc705a3a3ab50c1549e1b3c7cd3b6c0a7f
Hanjun-Cho <60430043+Hanjun-Cho@users.noreply.github.com>
2024-03-14 16:30:42 -0400

    made changes

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -15,14 +15,11 @@ import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
 import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
+import { Image, ImageCaption, ImageResize, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
 
@@ -41,18 +38,15 @@ export default class ClassicEditor extends ClassicEditorBase {
 		Heading,
 		Image,
 		ImageCaption,
+		ImageResize,
 		ImageStyle,
 		ImageToolbar,
 		ImageUpload,
 		Indent,
-		Link,
 		List,
-		MediaEmbed,
 		Paragraph,
 		PasteFromOffice,
 		PictureEditing,
-		Table,
-		TableToolbar,
 		TextTransformation
 	];
 
@@ -62,7 +56,7 @@ export default class ClassicEditor extends ClassicEditorBase {
 				'undo', 'redo',
 				'|', 'heading',
 				'|', 'bold', 'italic',
-				'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+				'|', 'uploadImage', 'blockQuote',
 				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
 			]
 		},
@@ -72,15 +66,7 @@ export default class ClassicEditor extends ClassicEditorBase {
 				'imageStyle:block',
 				'imageStyle:side',
 				'|',
-				'toggleImageCaption',
 				'imageTextAlternative'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
 			]
 		},
 		// This value must be kept in sync with the language defined in webpack.config.js.
